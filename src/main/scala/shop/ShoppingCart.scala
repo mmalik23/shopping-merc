addbd8e90119f255097325ccba53817f4ee1489b
Muqeet Malik <muqeetamalik@gmail.com>
2021-06-16 19:13:18 +0100

    Refactor: include add method in pence case class

--- src/main/scala/shop/ShoppingCart.scala
+++ src/main/scala/shop/ShoppingCart.scala
@@ -1,6 +1,8 @@
 package shop
 
-case class Pence(value: Int)
+case class Pence(value: Int) {
+    def add(p: Pence) =  this.copy(value + p.value)
+}
 
 sealed trait Item
 case object Apple extends Item
@@ -9,5 +11,5 @@ case object Orange extends Item
 class ShoppingCart {
     val inventory: Map[Item, Pence] = Map(Apple -> Pence(60), Orange -> Pence(25))
 
-    def checkout(items: List[Item]): Pence = Pence(items.flatMap(inventory.get).map(_.value).fold(0)(_ + _))
+    def checkout(items: List[Item]): Pence = items.flatMap(inventory.get).fold(Pence(0))(_.add(_))
 }
